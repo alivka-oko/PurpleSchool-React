84eff5fc99c17e9be5d0d2b1549e72f162d5993b
alivka-oko <alina.okotnikova2002@gmail.com>
2025-05-19 11:32:19 +0300

    Title

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import './App.css';
 import Button from '../components/Button/Button';
 import Paragraph from '../components/Paragraph/Paragraph';
+import Title from '../components/Title/Title';
 
 function App() {
 	return (
 		<>
+			<Title>Заголовок</Title>
+			<Paragraph size='small'>Здесь много букав размером поменьше</Paragraph>
 			<Paragraph>Стандартный параграф</Paragraph>
-			<Paragraph size="small">Здесь много букав размером поменьше</Paragraph>
 			<Button>Здесь текст</Button>
 		</>
 	);
