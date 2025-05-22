04ef9041b91414909c69d1b681c37b19d53bc9ca
alivka-oko <alina.okotnikova2002@gmail.com>
2025-05-22 12:38:08 +0300

    paragraph + search area module

--- components/Paragraph/Paragraph.jsx
+++ components/Paragraph/Paragraph.jsx
@@ -1,12 +1,10 @@
-import './Paragraph.css';
+import styles from './Paragraph.module.css';
+import cn from 'classnames';
 
 function Paragraph({ children, size }) {
-	let paragraphClass = size?`paragraph paragraph-${size}`: 'paragraph' ;
-	return (
-		<p className={paragraphClass}>
-			{children}
-		</p>
-	);
+	let paragraphClass = size ? `paragraph paragraph-${size}` : 'paragraph';
+	const classNameModule = paragraphClass.split(' ').map((className) => styles[className]);
+	return <p className={cn(classNameModule)}>{children}</p>;
 }
 
 export default Paragraph;
--- components/SearchArea/SearchArea.jsx
+++ components/SearchArea/SearchArea.jsx
@@ -1,28 +1,28 @@
-import './SearchArea.css';
-import Input from '../Input/Input';
-import Button from '../Button/Button';
-import Paragraph from '../Paragraph/Paragraph';
-import Title from '../Title/Title';
+import styles from "./SearchArea.module.css";
+import Input from "../Input/Input";
+import Button from "../Button/Button";
+import Paragraph from "../Paragraph/Paragraph";
+import Title from "../Title/Title";
 
 function SearchArea() {
-	const search = () => {
-		console.log('Кнопочка работает');
-	};
-	return (
-		<div className='search-area'>
-			<div className='search-area-title'>
-				<Title>Поиск</Title>
-				<Paragraph size='small'>
+  const search = () => {
+    console.log("Кнопочка работает");
+  };
+  return (
+    <div className='search-area'>
+      <div className='search-area-title'>
+        <Title>Поиск</Title>
+        <Paragraph size='small'>
           Введите название фильма, сериала или мультфильма для поиска и
           добавления в избранное.
-				</Paragraph>
-			</div>
-			<div className='search-area-input'>
-				<Input placeholder={'Введите название'} icon='search'></Input>
-				<Button text={'Искать'} onClick={search} />
-			</div>
-		</div>
-	);
+        </Paragraph>
+      </div>
+      <div className='search-area-input'>
+        <Input placeholder={"Введите название"} icon='search'></Input>
+        <Button text={"Искать"} onClick={search} />
+      </div>
+    </div>
+  );
 }
 
 export default SearchArea;
